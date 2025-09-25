39cc5f7e973df61af6404765a5251959ecad2951
Mich-roan <michelle.rodriguez@uteca.edu.mx>
2025-09-24 19:47:53 -0600

    Recolecta monedas

---
+++ Assets/Scripts/ClickRaycast.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickRaycast : MonoBehaviour
+{
+    [SerializeField]
+    private float raycastDistance = 100f;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    private string coinTag = "Coin";
+    [SerializeField]
+    private UnityEvent onCoinCollected;
+    [SerializeField]
+    private bool isActive = true;
+    private void SetActive(bool active)
+    {
+        isActive = active;
+    }
+    void Update()
+    {
+        if (isActive && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, raycastDistance, layerMask))
+            {
+                if (hitInfo.collider.CompareTag(coinTag))
+                {
+                    PressCoin(hitInfo.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void PressCoin(GameObject coin)
+    {
+        onCoinCollected.Invoke();
+        coin.GetComponent<Coin>().Collect();
+    }
+}
