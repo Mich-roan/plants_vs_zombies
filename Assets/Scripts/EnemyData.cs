a60147b7e94d5a4492539871ffbced5b338640d5
Mich-roan <michelle.rodriguez@uteca.edu.mx>
2025-10-07 18:51:18 -0600

    Enemigo y animación

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public float health = 100f;
+    public float speed = 5f;
+    public int damage = 10;
+    public string enemyName;
+    public string attackAnimation = "Atack";
+    public string deathAnimation = "Die";
+    public string walkAnimation = "Walk";
+    public string hitAnimation = "Hit";
+    public string winAnimation = "Win";
+    public string idleAnimation = "Idle";
+}
